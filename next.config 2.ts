e6e7866397966e3f7f06cdd23b699ca70498725e
George Smith <gsmith0572@gmail.com>
2026-03-12 19:59:41 -0500

    chore: remove all duplicate files with spaces

--- next.config 2.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.4.217', '*.loca.lt', 'we-the-people-39120.loca.lt'],
-};
-
-export default nextConfig;
